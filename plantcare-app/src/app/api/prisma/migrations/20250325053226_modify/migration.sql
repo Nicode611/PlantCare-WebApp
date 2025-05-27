c22ed5236cd411f8e3756e6acc3d6d16af39e007
SylverZ <127538225+Nicode611@users.noreply.github.com>
2025-05-27 22:54:01 +1000

    change to postgresql db

--- plantcare-app/src/app/api/prisma/migrations/20250325053226_modify/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `water_lvl_needed` on table `Model` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `Model` MODIFY `water_lvl_needed` INTEGER NOT NULL;
--- plantcare-app/src/app/api/prisma/migrations/20250423033649_next/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Plant` MODIFY `next_watering_date` DATETIME(3) NULL;
